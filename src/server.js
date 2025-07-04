a05335ebbc23346243034c3c4857b77a14a04442
LuisFelipeSalvarani <felipe_salvarani@hotmail.com>
2025-07-03 22:12:35 -0300

    Streams no Node.js - Criando ID único e universal (UUID)

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import http from 'node:http'
-import { json } from './middlewares/json.js'
+import { randomUUID } from 'node:crypto'
 import { Database } from './database.js'
+import { json } from './middlewares/json.js'
 
 // - Criar usuários
 // - Listar usuários
@@ -49,7 +50,7 @@ const server = http.createServer(async (req, res) => {
     const { name, email } = req.body
 
     const user = {
-      id: 1,
+      id: randomUUID(),
       name,
       email,
     }
