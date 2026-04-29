e61cfe4367dd27468673a43c42504e1f119fdedb
LuisFelipeSalvarani <felipe_salvarani@hotmail.com>
2026-04-29 14:36:49 -0300

    Implementando rotas - Plugins do Fastify

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,10 @@
 import fastify from 'fastify'
-import { knex } from './database.js'
 import { env } from './env/index.js'
+import { transactionsRoutes } from './routes/transactions.js'
 
 const app = fastify()
 
-app.get('/hello', async () => {
-  const transactions = await knex('transactions')
-    .where('amount', 100)
-    .select('*')
-
-  return transactions
-})
+app.register(transactionsRoutes)
 
 app
   .listen({
