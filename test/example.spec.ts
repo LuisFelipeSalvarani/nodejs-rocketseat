ba80fff1250039de70c640b1cad1aaff17495a7f
LuisFelipeSalvarani <felipe_salvarani@hotmail.com>
2026-05-05 14:23:40 -0300

    Testes automatizados - Testando criação de transação

--- test/example.spec.ts
+++ test/example.spec.ts
@@ -1,7 +1,22 @@
-import { expect, test } from 'vitest'
+import { afterAll, beforeAll, test } from 'vitest'
+import request from 'supertest'
+import { app } from '../src/app.js'
 
-test('o usuário consegue criar uma nova transação', () => {
-  const responseStatusCode = 201
+beforeAll(async () => {
+  await app.ready()
+})
 
-  expect(responseStatusCode).toEqual(201)
+afterAll(() => {
+  app.close()
+})
+
+test('user can create a new transaction', async () => {
+  await request(app.server)
+    .post('/transactions')
+    .send({
+      title: 'new transaction',
+      amount: 5000,
+      type: 'credit',
+    })
+    .expect(201)
 })
