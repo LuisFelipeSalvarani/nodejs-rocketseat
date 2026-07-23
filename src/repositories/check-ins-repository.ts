41d51df208819da6acc48e6ecb458569504719ce
LuisFelipeSalvarani <felipe_salvarani@hotmail.com>
2026-07-23 18:10:41 -0300

    Implementando casos de uso - TDD & Mocking

--- src/repositories/check-ins-repository.ts
+++ src/repositories/check-ins-repository.ts
@@ -3,4 +3,5 @@ import { CheckInUncheckedCreateInput } from '../../prisma/generated/prisma/model
 
 export interface CheckInsRepository {
   create(data: CheckInUncheckedCreateInput): Promise<CheckIn>
+  findByUserIdOnDate(userId: string, date: Date): Promise<CheckIn | null>
 }
