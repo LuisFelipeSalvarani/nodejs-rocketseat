1b5f2e6d462963d599d015db664ab709d98fd219
LuisFelipeSalvarani <felipe_salvarani@hotmail.com>
2026-07-23 17:16:25 -0300

    Implementando casos de uso - Caso de uso de perfil

--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -1,8 +1,13 @@
 import { prisma } from '@/lib/prisma'
 import { UserCreateInput } from '../../../prisma/generated/prisma/models'
 import { UsersRepository } from '../users-repository'
+import { User } from '../../../prisma/generated/prisma/browser'
 
 export class PrismaUsersRepository implements UsersRepository {
+  findById(id: string): Promise<User | null> {
+    throw new Error('Method not implemented.')
+  }
+
   async findByEmail(email: string) {
     const user = await prisma.user.findUnique({
       where: {
--- src/repositories/users-repository.ts
+++ src/repositories/users-repository.ts
@@ -2,6 +2,7 @@ import { User } from '../../prisma/generated/prisma/browser'
 import { UserCreateInput } from '../../prisma/generated/prisma/models'
 
 export interface UsersRepository {
+  findById(id: string): Promise<User | null>
   findByEmail(email: string): Promise<User | null>
   create(data: UserCreateInput): Promise<User>
 }
