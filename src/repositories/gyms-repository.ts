0f005184967392361b33e1c59eafe1b93881560b
LuisFelipeSalvarani <felipe_salvarani@hotmail.com>
2026-07-24 11:06:27 -0300

    Implementando casos de uso - Caso de uso de criação de academia

--- src/repositories/gyms-repository.ts
+++ src/repositories/gyms-repository.ts
@@ -1,5 +1,7 @@
 import { Gym } from '../../prisma/generated/prisma/client'
+import { GymCreateInput } from '../../prisma/generated/prisma/models'
 
 export interface GymsRepository {
   findById(id: string): Promise<Gym | null>
+  create(gym: GymCreateInput): Promise<Gym>
 }
--- src/repositories/in-memory/in-memory-gyms-repository.ts
+++ src/repositories/in-memory/in-memory-gyms-repository.ts
@@ -1,5 +1,8 @@
+import { randomUUID } from 'node:crypto'
 import { Gym } from '../../../prisma/generated/prisma/client'
+import { GymCreateInput } from '../../../prisma/generated/prisma/models'
 import { GymsRepository } from '../gyms-repository'
+import { Decimal } from '@prisma/client/runtime/client'
 
 export class InMemoryGymsRepository implements GymsRepository {
   public items: Gym[] = []
@@ -10,6 +13,22 @@ export class InMemoryGymsRepository implements GymsRepository {
     if (!gym) {
       return null
     }
+
+    return gym
+  }
+
+  async create(data: GymCreateInput) {
+    const gym = {
+      id: data.id ?? randomUUID(),
+      title: data.title,
+      description: data.description ?? null,
+      phone: data.phone ?? null,
+      latitude: new Decimal(data.latitude.toString()),
+      longitude: new Decimal(data.longitude.toString()),
+      created_at: new Date(),
+    }
+
+    this.items.push(gym)
 
     return gym
   }
