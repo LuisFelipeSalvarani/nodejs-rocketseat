14753725701bf2f7e79604f8ac9ea856bc367339
LuisFelipeSalvarani <felipe_salvarani@hotmail.com>
2026-07-24 13:29:08 -0300

    Implementando casos de uso - Caso de uso de busca de academias

--- src/repositories/gyms-repository.ts
+++ src/repositories/gyms-repository.ts
@@ -3,5 +3,6 @@ import { GymCreateInput } from '../../prisma/generated/prisma/models'
 
 export interface GymsRepository {
   findById(id: string): Promise<Gym | null>
+  searchMany(query: string, page: number): Promise<Gym[]>
   create(gym: GymCreateInput): Promise<Gym>
 }
