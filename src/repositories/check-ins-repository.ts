d92c846aefc6b549b4ff8d9390bf5dc93c17505f
LuisFelipeSalvarani <felipe_salvarani@hotmail.com>
2026-07-24 12:05:21 -0300

    Implementando casos de uso - Caso de uso de métricas

--- src/repositories/check-ins-repository.ts
+++ src/repositories/check-ins-repository.ts
@@ -4,5 +4,6 @@ import { CheckInUncheckedCreateInput } from '../../prisma/generated/prisma/model
 export interface CheckInsRepository {
   findByUserIdOnDate(userId: string, date: Date): Promise<CheckIn | null>
   findManyByUserId(userId: string, page: number): Promise<CheckIn[]>
+  countByUserId(userId: string): Promise<number>
   create(data: CheckInUncheckedCreateInput): Promise<CheckIn>
 }
