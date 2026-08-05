b69c0fccf8bc50613e18e55cfbbd136e78328cf9
LuisFelipeSalvarani <felipe_salvarani@hotmail.com>
2026-08-05 17:23:49 -0300

    Fórum com DDD & Clean Architecture - Configuração do ESLint

--- src/domain/entities/instructor.ts
+++ src/domain/entities/instructor.ts
@@ -7,10 +7,13 @@ interface InstructorProps {
 
 export class Instructor extends Entity<InstructorProps> {
   static create(props: InstructorProps, id?: UniqueEntityID) {
-    const instructor = new Instructor({
-      ...props,
-    }, id)
-  
+    const instructor = new Instructor(
+      {
+        ...props,
+      },
+      id,
+    )
+
     return instructor
   }
 }
--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,8 +1,8 @@
-import { Slug } from "./value-objects/slug"
-import { Entity } from "@/core/entities/entity"
-import { UniqueEntityID } from "@/core/entities/unique-entity-id"
-import { Optional } from "@/core/types/optional"
-import dayjs from "dayjs"
+import { Slug } from './value-objects/slug'
+import { Entity } from '@/core/entities/entity'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { Optional } from '@/core/types/optional'
+import dayjs from 'dayjs'
 
 interface QuestionProps {
   authorId: UniqueEntityID
@@ -15,10 +15,10 @@ interface QuestionProps {
 }
 
 export class Question extends Entity<QuestionProps> {
-    get authorId() {
+  get authorId() {
     return this.props.authorId
   }
-  
+
   get bestAnswerId() {
     return this.props.bestAnswerId
   }
@@ -72,12 +72,18 @@ export class Question extends Entity<QuestionProps> {
     this.touch()
   }
 
-  static create(props: Optional<QuestionProps, 'createdAt' | 'slug'>, id?: UniqueEntityID) {
-    const question = new Question({
-      ...props,
-      slug: props.slug ?? Slug.createFromText(props.title),
-      createdAt: new Date()
-    }, id)
+  static create(
+    props: Optional<QuestionProps, 'createdAt' | 'slug'>,
+    id?: UniqueEntityID,
+  ) {
+    const question = new Question(
+      {
+        ...props,
+        slug: props.slug ?? Slug.createFromText(props.title),
+        createdAt: new Date(),
+      },
+      id,
+    )
 
     return question
   }
