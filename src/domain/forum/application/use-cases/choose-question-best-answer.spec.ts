5e9338581625aafdb1e2640c5e1387a352e68a57
LuisFelipeSalvarani <felipe_salvarani@hotmail.com>
2026-08-06 12:05:27 -0300

    Fórum com DDD & Clean Architecture - Caso de uso: Deletar comentário da pergunta

--- src/domain/forum/application/use-cases/choose-question-best-answer.spec.ts
+++ src/domain/forum/application/use-cases/choose-question-best-answer.spec.ts
@@ -36,7 +36,7 @@ describe('Choose Question Best Answer', () => {
     expect(inMemoryQuestionsRepository.items[0].bestAnswerId).toEqual(answer.id)
   })
 
-  it('should be able to choose another user question best answer', async () => {
+  it('should not be able to choose another user question best answer', async () => {
     const question = makeQuestion({
       authorId: new UniqueEntityID('author-1'),
     })
