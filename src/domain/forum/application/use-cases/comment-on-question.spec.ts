4f8d6daaa40767986603de5608d3fd8f47de1144
LuisFelipeSalvarani <felipe_salvarani@hotmail.com>
2026-08-10 15:40:22 -0300

    Aggregates & Watched Lists - Excluindo anexos da pergunta

--- src/domain/forum/application/use-cases/comment-on-question.spec.ts
+++ src/domain/forum/application/use-cases/comment-on-question.spec.ts
@@ -2,14 +2,20 @@ import { InMemoryQuestionsRepository } from '../../../../../test/repositories/in
 import { InMemoryQuestionCommentsRepository } from '../../../../../test/repositories/in-memory-question-comments-repository'
 import { CommentOnQuestionUseCase } from './comment-on-question'
 import { makeQuestion } from '../../../../../test/factories/make-question'
+import { InMemoryQuestionAttachmentsRepository } from '../../../../../test/repositories/in-memory-question-attachment-repository'
 
 let inMemoryQuestionsRepository: InMemoryQuestionsRepository
 let inMemoryQuestionCommentsRepository: InMemoryQuestionCommentsRepository
+let inMemoryQuestionAttachmentsRepository: InMemoryQuestionAttachmentsRepository
 let sut: CommentOnQuestionUseCase
 
 describe('Comment on Question', () => {
   beforeEach(() => {
-    inMemoryQuestionsRepository = new InMemoryQuestionsRepository()
+    inMemoryQuestionAttachmentsRepository =
+      new InMemoryQuestionAttachmentsRepository()
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository(
+      inMemoryQuestionAttachmentsRepository,
+    )
     inMemoryQuestionCommentsRepository =
       new InMemoryQuestionCommentsRepository()
     sut = new CommentOnQuestionUseCase(
