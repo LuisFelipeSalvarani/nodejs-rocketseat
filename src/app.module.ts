61d1337bd5b01f7cccfba0a291b24209da46abf3
LuisFelipeSalvarani <felipe_salvarani@hotmail.com>
2026-08-15 14:33:04 -0300

    Fundamentos do Nest - Testes E2E de perguntas

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,12 @@
 import { Module } from "@nestjs/common"
 import { ConfigModule } from "@nestjs/config"
-import { AuthModule } from "@/auth/auth.module.js"
-import { AuthenticationController } from "@/controllers/authenticate.controller.js"
-import { CreateAccountController } from "@/controllers/create-account.controller.js"
-import { CreateQuestionController } from "@/controllers/create-question.controller.js"
-import { FetchRecentQuestionController } from "@/controllers/fetch-recent-questions.controller.js"
-import { envSchema } from "@/env.js"
-import { PrismaService } from "@/prisma/prisma.service.js"
+import { AuthModule } from "./auth/auth.module.js"
+import { AuthenticationController } from "./controllers/authenticate.controller.js"
+import { CreateAccountController } from "./controllers/create-account.controller.js"
+import { CreateQuestionController } from "./controllers/create-question.controller.js"
+import { FetchRecentQuestionController } from "./controllers/fetch-recent-questions.controller.js"
+import { envSchema } from "./env.js"
+import { PrismaService } from "./prisma/prisma.service.js"
 
 @Module({
   imports: [
