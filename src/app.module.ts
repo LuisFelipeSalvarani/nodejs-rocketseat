51ce099e7e047d8a27998d5ddf977be34ab26e9f
LuisFelipeSalvarani <felipe_salvarani@hotmail.com>
2026-08-15 13:57:57 -0300

    Fundamentos do Nest - Testes E2E de usuários

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from "@nestjs/common"
 import { ConfigModule } from "@nestjs/config"
 import { AuthModule } from "@/auth/auth.module.js"
-import { AuthenticationController } from "@/controllers/authenticate-.controller.js"
+import { AuthenticationController } from "@/controllers/authenticate.controller.js"
 import { CreateAccountController } from "@/controllers/create-account.controller.js"
 import { CreateQuestionController } from "@/controllers/create-question.controller.js"
 import { FetchRecentQuestionController } from "@/controllers/fetch-recent-questions.controller.js"
