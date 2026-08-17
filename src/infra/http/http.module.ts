262b0c27f9813ddbe8047f62f2f17d505638dafa
LuisFelipeSalvarani <felipe_salvarani@hotmail.com>
2026-08-17 17:39:52 -0300

    Desacoplando camadas no NestJS - Criando camada de Infraestrutura

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,21 +1,11 @@
 import { Module } from "@nestjs/common"
-import { ConfigModule } from "@nestjs/config"
-import { AuthModule } from "./auth/auth.module.js"
+import { PrismaService } from "../prisma/prisma.service.js"
 import { AuthenticationController } from "./controllers/authenticate.controller.js"
 import { CreateAccountController } from "./controllers/create-account.controller.js"
 import { CreateQuestionController } from "./controllers/create-question.controller.js"
 import { FetchRecentQuestionController } from "./controllers/fetch-recent-questions.controller.js"
-import { envSchema } from "./env.js"
-import { PrismaService } from "./prisma/prisma.service.js"
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      validate: (env) => envSchema.parse(env),
-      isGlobal: true,
-    }),
-    AuthModule,
-  ],
   controllers: [
     CreateAccountController,
     AuthenticationController,
@@ -24,4 +14,4 @@ import { PrismaService } from "./prisma/prisma.service.js"
   ],
   providers: [PrismaService],
 })
-export class AppModule {}
+export class HttpModule {}
