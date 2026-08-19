1bcb39e982b221d74c4375ad5d6106517ec5762e
LuisFelipeSalvarani <felipe_salvarani@hotmail.com>
2026-08-19 15:53:16 -0300

    Desacoplando camadas no NestJS - Stubs de Criptografia

---
+++ test/cryptography/fake-hasher.ts
@@ -0,0 +1,12 @@
+import { HashComparer } from "../../src/domain/forum/application/cryptography/hash-comparer.js"
+import { HashGenerator } from "../../src/domain/forum/application/cryptography/hash-generator.js"
+
+export class FakeHasher implements HashGenerator, HashComparer {
+  async hash(plain: string) {
+    return plain.concat("-hashed")
+  }
+
+  async compare(plain: string, hash: string) {
+    return plain.concat("-hashed") === hash
+  }
+}
