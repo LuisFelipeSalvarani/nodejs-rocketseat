d24aba8067a6ebc4e726a3fff3381353de6f089e
LuisFelipeSalvarani <felipe_salvarani@hotmail.com>
2026-08-19 10:11:23 -0300

    Desacoplando camadas no NestJS - Presenter de perguntas

--- src/infra/http/controllers/fetch-recent-questions.controller.ts
+++ src/infra/http/controllers/fetch-recent-questions.controller.ts
@@ -3,6 +3,7 @@ import { z } from "zod"
 import { FetchRecentQuestionsUseCase } from "../../../domain/forum/application/use-cases/fetch-recent-questions.js"
 import { JwtAuthGuard } from "../../auth/jwt-auth.guard.js"
 import { ZodValidationPipe } from "../pipes/zod-validation-pipe.js"
+import { QuestionPresenter } from "../presenters/question-presenter.js"
 
 const pageQueryParamSchema = z
   .string()
@@ -22,10 +23,16 @@ export class FetchRecentQuestionController {
 
   @Get()
   async handle(@Query("page", queryValidationPipe) page: PageQueryParamSchema) {
-    const questions = await this.fetchRecentQuestions.execute({
+    const result = await this.fetchRecentQuestions.execute({
       page,
     })
 
-    return { questions }
+    if (result.isLeft()) {
+      throw new Error()
+    }
+
+    const { questions } = result.value
+
+    return { questions: questions.map(QuestionPresenter.toHTTP) }
   }
 }
