ebb6172c61c0da614a08e4e5ec012277bce2cf0d
LuisFelipeSalvarani <felipe_salvarani@hotmail.com>
2026-09-05 12:17:25 -0300

    Uploads e Relacionamentos - Editando perguntas com anexos

--- src/infra/http/controllers/edit-question.controller.ts
+++ src/infra/http/controllers/edit-question.controller.ts
@@ -15,6 +15,7 @@ import { ZodValidationPipe } from "../pipes/zod-validation-pipe.js"
 const editQuestionBodySchema = z.object({
   title: z.string(),
   content: z.string(),
+  attachments: z.array(z.uuid()),
 })
 
 const bodyValidationPipe = new ZodValidationPipe(editQuestionBodySchema)
@@ -32,14 +33,14 @@ export class EditQuestionController {
     @CurrentUser() user: UserPayload,
     @Param("id") questionId: string
   ) {
-    const { title, content } = body
+    const { title, content, attachments } = body
     const authorId = user.sub
 
     const result = await this.editQuestion.execute({
       title,
       content,
       authorId,
-      attachmentsIds: [],
+      attachmentsIds: attachments,
       questionId,
     })
 
--- test/factories/make-question-attachment.ts
+++ test/factories/make-question-attachment.ts
@@ -1,8 +1,10 @@
+import { Injectable } from "@nestjs/common"
 import { UniqueEntityID } from "../../src/core/entities/unique-entity-id.js"
 import {
   QuestionAttachment,
   QuestionAttachmentProps,
 } from "../../src/domain/forum/enterprise/entities/question-attachment.js"
+import { PrismaService } from "../../src/infra/database/prisma/prisma.service.js"
 
 export function makeQuestionAttachment(
   override: Partial<QuestionAttachmentProps> = {},
@@ -18,4 +20,26 @@ export function makeQuestionAttachment(
   )
 
   return questionAttachment
+}
+
+@Injectable()
+export class QuestionAttachmentFactory {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async makePrismaQuestionAttachment(
+    data: Partial<QuestionAttachmentProps> = {}
+  ): Promise<QuestionAttachment> {
+    const questionAttachment = makeQuestionAttachment(data)
+
+    await this.prisma.attachment.update({
+      where: {
+        id: questionAttachment.attachmentId.toString(),
+      },
+      data: {
+        questionId: questionAttachment.questionId.toString(),
+      },
+    })
+
+    return questionAttachment
+  }
 }
