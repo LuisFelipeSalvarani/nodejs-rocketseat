f43988d785142947f816f2a143bff04df16b7232
LuisFelipeSalvarani <felipe_salvarani@hotmail.com>
2026-09-08 17:13:05 -0300

    Uploads e Relacionamentos - Listando comentários com autor

--- src/domain/forum/application/use-cases/fetch-question-comments.ts
+++ src/domain/forum/application/use-cases/fetch-question-comments.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@nestjs/common"
 import { Either, right } from "../../../../core/either.js"
-import { QuestionComment } from "../../enterprise/entities/question-comment.js"
+import { CommentWithAuthor } from "../../enterprise/entities/value-objects/comment-with-author.js"
 import { QuestionCommentsRepository } from "../repositories/question-comments-repository.js"
 
 interface FetchQuestionCommentsUseCaseRequest {
@@ -11,7 +11,7 @@ interface FetchQuestionCommentsUseCaseRequest {
 type FetchQuestionCommentsUseCaseResponse = Either<
   null,
   {
-    questionComments: QuestionComment[]
+    comments: CommentWithAuthor[]
   }
 >
 
@@ -25,11 +25,14 @@ export class FetchQuestionCommentsUseCase {
     questionId,
     page,
   }: FetchQuestionCommentsUseCaseRequest): Promise<FetchQuestionCommentsUseCaseResponse> {
-    const questionComments =
-      await this.questionCommentRepository.findManyByQuestionId(questionId, {
-        page,
-      })
+    const comments =
+      await this.questionCommentRepository.findManyByQuestionIdWithAuthor(
+        questionId,
+        {
+          page,
+        }
+      )
 
-    return right({ questionComments })
+    return right({ comments })
   }
 }
--- test/repositories/in-memory-question-comments-repository.ts
+++ test/repositories/in-memory-question-comments-repository.ts
@@ -1,11 +1,15 @@
 import { PaginationParams } from "../../src/core/repositories/pagination-params.js"
 import { QuestionCommentsRepository } from "../../src/domain/forum/application/repositories/question-comments-repository.js"
 import { QuestionComment } from "../../src/domain/forum/enterprise/entities/question-comment.js"
+import { CommentWithAuthor } from "../../src/domain/forum/enterprise/entities/value-objects/comment-with-author.js"
+import { InMemoryStudentsRepository } from "./in-memory-students-repository.js"
 
 export class InMemoryQuestionCommentsRepository
   implements QuestionCommentsRepository
 {
   items: QuestionComment[] = []
+
+  constructor(readonly studentsRepository: InMemoryStudentsRepository) {}
 
   async findById(id: string) {
     const questionComment = this.items.find((item) => item.id.toString() === id)
@@ -21,6 +25,37 @@ export class InMemoryQuestionCommentsRepository
     const questionComments = this.items
       .filter((item) => item.questionId.toString() === questionId)
       .slice((page - 1) * 20, page * 20)
+
+    return questionComments
+  }
+
+  async findManyByQuestionIdWithAuthor(
+    questionId: string,
+    { page }: PaginationParams
+  ) {
+    const questionComments = this.items
+      .filter((item) => item.questionId.toString() === questionId)
+      .slice((page - 1) * 20, page * 20)
+      .map((comment) => {
+        const author = this.studentsRepository.items.find((student) =>
+          student.id.equals(comment.authorId)
+        )
+
+        if (!author) {
+          throw new Error(
+            `Author with ID "${comment.authorId.toString()}" does not exist.`
+          )
+        }
+
+        return CommentWithAuthor.create({
+          content: comment.content,
+          commentId: comment.id,
+          createdAt: comment.createdAt,
+          updatedAt: comment.updatedAt,
+          authorId: comment.authorId,
+          author: author.name,
+        })
+      })
 
     return questionComments
   }
