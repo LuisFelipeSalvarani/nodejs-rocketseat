d6a8cba37936a412bb4aa1f4f893ab7192a79fad
LuisFelipeSalvarani <felipe_salvarani@hotmail.com>
2026-09-09 18:06:50 -0300

    Uploads e Relacionamentos - Controller comentário com autor

--- src/infra/http/controllers/fetch-question-comments.controller.ts
+++ src/infra/http/controllers/fetch-question-comments.controller.ts
@@ -8,7 +8,7 @@ import {
 import { z } from "zod"
 import { FetchQuestionCommentsUseCase } from "../../../domain/forum/application/use-cases/fetch-question-comments.js"
 import { ZodValidationPipe } from "../pipes/zod-validation-pipe.js"
-import { CommentPresenter } from "../presenters/comment-presenter.js"
+import { CommentWithAuthorPresenter } from "../presenters/comment-with-author-presenter.js"
 
 const pageQueryParamSchema = z
   .string()
@@ -39,8 +39,8 @@ export class FetchQuestionCommentsController {
       throw new BadRequestException()
     }
 
-    const { questionComments } = result.value
+    const { comments } = result.value
 
-    return { comments: questionComments.map(CommentPresenter.toHTTP) }
+    return { comments: comments.map(CommentWithAuthorPresenter.toHTTP) }
   }
 }
