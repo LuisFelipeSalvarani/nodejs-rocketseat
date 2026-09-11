15b83904550070c23309621f75628cf0620b8360
LuisFelipeSalvarani <felipe_salvarani@hotmail.com>
2026-09-11 13:34:07 -0300

    Eventos de Domínio & Cache - Implementando Cache com Redis

--- src/infra/cache/cache.module.ts
+++ src/infra/cache/cache.module.ts
@@ -1,9 +1,18 @@
 import { Module } from "@nestjs/common"
 import { EnvModule } from "../env/env.module.js"
-import { EnvService } from "../env/env.service.js"
+import { CacheRepository } from "./cache-repository.js"
+import { RedisService } from "./redis/redis.service.js"
+import { RedisCacheRepository } from "./redis/redis-cache-repository.js"
 
 @Module({
   imports: [EnvModule],
-  providers: [EnvService],
+  providers: [
+    RedisService,
+    {
+      provide: CacheRepository,
+      useClass: RedisCacheRepository,
+    },
+  ],
+  exports: [CacheRepository],
 })
 export class CacheModule {}
